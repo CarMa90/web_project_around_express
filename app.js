06e1a95cf5eacb1aa62b1cef3543175d2eb3119f
CarMa90 <el.matteotti@gmail.com>
2026-06-03 19:16:54 -0700

    feature initial configurations

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const app = express();
+
+const { PORT = 3000, BASE_PATH } = process.env;
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
